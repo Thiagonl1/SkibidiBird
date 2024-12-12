d9573a71c9c83db028e2df0e0d5d74ee05ed4f6a
Hello world <somnii006@gmail.com>
2024-12-12 00:23:08 -0300

    uy trabaje en el main

--- flappy.gd
+++ flappy.gd
@@ -2,24 +2,32 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = 500.0
+const JUMP_VELOCITY = -500.0
 
-
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var camera = $Camera2D
+func _process(delta: float) -> void:
+	camera.position.y = 0
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
+	print(rotation_degrees)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if(rotation_degrees <= 70):
+			rotation_degrees = rotation_degrees + 2
+		else:
+			rotation_degrees = 70
+	if(rotation_degrees < -50):
+		animated_sprite.play("flap")
+	else:
+		animated_sprite.play("flying")
+	camera.position.y = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Saltar"):
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		rotation_degrees = -70
+	velocity.x = SPEED
 	move_and_slide()
